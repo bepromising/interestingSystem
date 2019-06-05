97fdf429768c0d9b1d1860415e6d0a187b8bbe31
cxi <dream_cxi@163.com>
2019-06-06 01:27:42 +0800

    增加eslint+prettier

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import './style/index.less';
-console.log(123);
+"use strict";
+import "@/style/index.less";
+console.log(a);
+__cxi
+let a = [{ a: "2" }, { b: "1" }];
+a.filter(vo => {
+  vo.a === 2;
+})
--- webpackConf/webpack.base.js
+++ webpackConf/webpack.base.js
@@ -1,65 +1,69 @@
-let webpack = require('webpack');
-let path = require('path');
-let HtmlWebpackPlugin = require('html-webpack-plugin');
-let {CleanWebpackPlugin} = require('clean-webpack-plugin');
-let CopyWebpackPlugin = require('copy-webpack-plugin');
-let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+let webpack = require("webpack")
+let path = require("path")
+let HtmlWebpackPlugin = require("html-webpack-plugin")
+let { CleanWebpackPlugin } = require("clean-webpack-plugin")
+let CopyWebpackPlugin = require("copy-webpack-plugin")
+let MiniCssExtractPlugin = require("mini-css-extract-plugin")
+console.log(process.cwd() + "/src")
 module.exports = {
-    entry: {
-        index: './src/index.js'
-    },
-    output: {
-        filename: '[name].[hash:8].js',
-        path: path.resolve(__dirname, '../dist/js')
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: 'index.html',
-            inject: true,
-            hash: true,
-            minify: {
-                collapseWhitespace: true, // 去掉空行
-                removeComments: true //去掉注释
-            }
-        }),
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin([
-            { from: './public/favico.png', to: './'}
-        ]),
-        new MiniCssExtractPlugin({
-            filename: 'css/main.css'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.(css|less)$/,
-                use: [MiniCssExtractPlugin.loader,'css-loader','postcss-loader','less-loader']
-            },
-            {
-                test: /\.(png|gif|jpg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 100*1024, // 小于 100kb 转为 base64-url
-                            outputPath: 'img/'
-                        }
-                    }
-                ]
-            }
+  entry: {
+    index: "./src/index.js"
+  },
+  output: {
+    filename: "[name].[hash:8].js",
+    path: process.cwd() + "/dist/js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+      inject: true,
+      hash: true,
+      minify: {
+        collapseWhitespace: true, // 去掉空行
+        removeComments: true // 去掉注释
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([{ from: "./public/favico.png", to: "./" }]),
+    new MiniCssExtractPlugin({
+      filename: "css/main.css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(css|less)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "less-loader"
         ]
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.tsx', '.css', '.json'],
-        alias: {
-            '@/': '../src/' // 路径别名
-        }
+      },
+      {
+        test: /\.(png|gif|jpg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 100 * 1024, // 小于 100kb 转为 base64-url
+              outputPath: "img/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".css", ".json"],
+    alias: {
+      "@": process.cwd() + "/src"
     }
-}
+  }
+};
