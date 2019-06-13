46508597b8f3b034c7aecffd42bcd253408a7791
cxi <dream_cxi@163.com>
2019-06-14 00:39:52 +0800

    fixed.

--- webpackConf/webpack.dev.js
+++ webpackConf/webpack.dev.js
@@ -1,6 +1,5 @@
 let { smart } = require('webpack-merge');
 let base = require('./webpack.base.js');
-// let path = require('path');
 module.exports = smart(base, {
 	mode: 'development',
 	devtool: 'cheap-eval-source-map',
