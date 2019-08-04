14f252f0c641e2b0d74b7a43ac6d44bef94d8753
cxi <dream_cxi@163.com>
2019-08-04 22:00:11 +0800

    代码雨效果

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,69 @@
 import '@/style/index';
-let app = document.querySelector('#app');
-if (app) app.innerHTML = 'chensy';
+class HackCanvas {
+	fontSize: number;
+	list: number[];
+	ctx: any;
+	width: number;
+	height: number;
+	timer: any;
+	canvas: any;
+
+	constructor(id: string) {
+		// @ts-ignore
+		this.canvas = document.getElementById(id);
+		// @ts-ignore
+		this.ctx = this.canvas.getContext('2d');
+		this.fontSize = 16;
+		this.list = [];
+		this.timer = 0;
+		this.width = 0;
+		this.height = 0;
+	}
+	setWidthAndHeight() {
+		let W = document.documentElement.clientWidth,
+			H = document.documentElement.clientHeight;
+		this.canvas.width = this.width = W;
+		this.canvas.height = this.height = H;
+	}
+	draw() {
+		this.ctx.fillStyle = 'rgba(0,0,0,0.05)';
+		this.ctx.fillRect(0, 0, this.width, this.height);
+		this.ctx.fillStyle = '#00ff00';
+		this.ctx.font = this.fontSize + 'px arial';
+		let columns = Math.ceil(this.width / this.fontSize);
+		for (let i = 0; i < columns; i++) {
+			let n = Math.floor(Math.random() * 10);
+			this.ctx.fillText(
+				n,
+				i * this.fontSize,
+				this.list[i] * this.fontSize
+			);
+			if (
+				this.list[i] * this.fontSize > this.height ||
+				Math.random() > 0.88
+			) {
+				this.list[i] = 0;
+			}
+			this.list[i]++;
+		}
+	}
+	init() {
+		this.setWidthAndHeight();
+		this.timer = setInterval(this.draw.bind(this), 66);
+	}
+	resize() {
+		clearInterval(this.timer);
+		this.setWidthAndHeight();
+		this.list = [];
+		this.init();
+	}
+}
+
+let app = new HackCanvas('canvas');
+
+window.onload = () => {
+	app.init();
+};
+window.onresize = () => {
+	app.resize();
+};
